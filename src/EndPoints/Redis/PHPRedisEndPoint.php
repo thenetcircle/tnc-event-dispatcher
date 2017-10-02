0d49791fc7d0631f6d178a884268e2d46229a896
benn <benn@thenetcircle.com>
2017-10-02 15:25:25 +0800

    add License

--- src/EndPoints/Redis/PHPRedisEndPoint.php
+++ src/EndPoints/Redis/PHPRedisEndPoint.php
@@ -1,11 +1,19 @@
 <?php
-
 /*
- * This file is part of the Tnc package.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * (c) Service Team
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * file that was distributed with this source code.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Beineng Ma <baineng.ma@gmail.com>
  */
 
 namespace TNC\EventDispatcher\EndPoints\Redis;
